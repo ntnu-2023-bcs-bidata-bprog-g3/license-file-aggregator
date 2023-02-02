7e32f9ac02859a0c424445b5b8a2f7bc39abf5d6
Kristian Svanholm <KristianRorenSvanholm@gmail.com>
2023-02-02 12:34:45 +0100

    Implement getLicense and some helper functions

--- src/api/API.cpp
+++ src/api/API.cpp
@@ -19,15 +19,61 @@ crow::response license(crow::request req){
     return crow::response(map2JSON(pool));
 }
 
+crow::response getLicense(crow::request req){
+    using namespace std;
 
-int poolSubtract(std::string type, int time){
-    return pool[type] - time > 0 ? time : 0;
+    const string params[] = {"id"};
+    map<string,string> vals = param2map(req.url_params, params, params->length());
+    if(!inMap(vals, (string)"id"))
+        return crow::response(map2JSON(pool));
+    return crow::response(inMap(pool, vals["id"]) ? val2JSON(vals["id"], pool[vals["id"]]) : 404);
 }
 
 template <typename T>
+void print(T str){
+    std::cout << str << std::endl;
+}
+
+int poolSubtract(const std::string type, const int time){
+    return pool[type] - time > 0 ? time : 0;
+}
+
+
+//####################################
+// TODO:: Separate into own file
+template <typename T>
 crow::json::wvalue map2JSON(std::map<std::string, T> map){
     crow::json::wvalue json;
-    for(const auto x: pool)
+    for(const auto x: map)
         json[x.first] = x.second;
     return json;
+}
+
+template <typename T>
+crow::json::wvalue val2JSON(const std::string key, const T val){
+    crow::json::wvalue json;
+    json[key] = val;
+    return json;
+}
+
+// Checks if key is in map where key and value is not the same
+template <typename T, typename U>
+bool inMap(std::map<U, T> map, const U key){
+    return map.find(key) != map.end();
+}
+
+template <typename T>
+bool inMap(std::map<T, T> map, const T key){
+    return map.find(key) != map.end();
+}
+
+// Parse url params and get all values into map.
+std::map<std::string,std::string> param2map(crow::query_string url, const std::string params[], const int size){
+    std::map<std::string, std::string> vals;
+    for(int i = 0; i<size-1; i++){
+        const auto param = params[i];
+        if(url.get(param) == nullptr) continue;
+        vals[param] = url.get(param);
+    }
+    return vals;
 }
