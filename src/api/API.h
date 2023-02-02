d0c48b6e6903972ee80a2e9dfec98eba633521b8
Kristian Svanholm <KristianRorenSvanholm@gmail.com>
2023-02-02 12:34:23 +0100

    Header file for API.cpp

--- src/api/API.h
+++ src/api/API.h
@@ -3,8 +3,19 @@
 
 #include "crow.h"
 
-int poolSubtract(std::string type, int time);
-template <typename T> crow::json::wvalue map2JSON(std::map<std::string, T> map);
 crow::response license(crow::request req);
+crow::response getLicense(crow::request req);
+
+int poolSubtract(std::string type, int time);
+template <typename T> void print(T str);
+
+//####################################
+// TODO:: Separate into own file
+template <typename T> crow::json::wvalue map2JSON(std::map<std::string, T> map);
+template <typename T> crow::json::wvalue val2JSON(const std::string key, const T val);
+template <typename T> bool inMap(std::map<T, T> map, T key);
+template <typename T, typename U> bool inMap(std::map<U, T> map, U key);
+
+std::map<std::string,std::string> param2map(crow::query_string url, const std::string params[], const int size);
 
 #endif
