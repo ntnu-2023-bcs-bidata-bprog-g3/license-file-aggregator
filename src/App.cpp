5eb9b9be8a5eb7ab382b76698b85288d8b3b3636
Kristian Svanholm <KristianRorenSvanholm@gmail.com>
2023-02-07 13:47:42 +0100

    Update indents on app.cpp

--- src/App.cpp
+++ src/App.cpp
@@ -10,28 +10,28 @@ std::map<std::string, int> pool;
 
 void run() {
 
-  /* Register Components in scope of run() method */
-  AppComponent components;
+	/* Register Components in scope of run() method */
+	AppComponent components;
 
-  /* Get router component */
-  OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router);
+	/* Get router component */
+	OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router);
 
-  /* Create MyController and add all of its endpoints to router */
-  router->addController(std::make_shared<Controller>());
+	/* Create MyController and add all of its endpoints to router */
+	router->addController(std::make_shared<Controller>());
 
-  /* Get connection handler component */
-  OATPP_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, connectionHandler);
+	/* Get connection handler component */
+	OATPP_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, connectionHandler);
 
-  /* Get connection provider component */
-  OATPP_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, connectionProvider);
+	/* Get connection provider component */
+	OATPP_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, connectionProvider);
 
-  /* Create server which takes provided TCP connections and passes them to HTTP connection handler */
-  oatpp::network::Server server(connectionProvider, connectionHandler);
+	/* Create server which takes provided TCP connections and passes them to HTTP connection handler */
+	oatpp::network::Server server(connectionProvider, connectionHandler);
 
-  /* Print info about server port */
-  OATPP_LOGI("License_consumer", "Server running on port %s", connectionProvider->getProperty("port").getData());
+	/* Print info about server port */
+	OATPP_LOGI("License_consumer", "Server running on port %s", connectionProvider->getProperty("port").getData());
 
-  server.run();
+	server.run();
   
 }
 
@@ -40,19 +40,19 @@ void run() {
  */
 int main(int argc, const char * argv[]) {
 
-  readPoolFromFile(&pool);
+	readPoolFromFile(&pool);
 
-  oatpp::base::Environment::init();
+	oatpp::base::Environment::init();
 
-  run();
-    
-  /* Print how much objects were created during app running, and what have left-probably leaked */
-  /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
-  std::cout << "\nEnvironment:\n";
-  std::cout << "objectsCount = " << oatpp::base::Environment::getObjectsCount() << "\n";
-  std::cout << "objectsCreated = " << oatpp::base::Environment::getObjectsCreated() << "\n\n";
-  
-  oatpp::base::Environment::destroy();
-  
-  return 0;
+	run();
+		
+	/* Print how much objects were created during app running, and what have left-probably leaked */
+	/* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
+	std::cout << "\nEnvironment:\n";
+	std::cout << "objectsCount = " << oatpp::base::Environment::getObjectsCount() << "\n";
+	std::cout << "objectsCreated = " << oatpp::base::Environment::getObjectsCreated() << "\n\n";
+	
+	oatpp::base::Environment::destroy();
+	
+	return 0;
 }
