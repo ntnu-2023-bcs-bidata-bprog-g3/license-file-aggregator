38e9b223a88871751e73e4360ff0136d6586659b
Kristian Svanholm <KristianRorenSvanholm@gmail.com>
2023-02-07 11:25:54 +0100

    File handlers for pool data

---
+++ src/file/fileHandler.hpp
@@ -0,0 +1,12 @@
+#ifndef fileHandler_HPP
+#define fileHandler_HPP
+
+#include <map>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+void writePoolToFile(std::map<std::string, int> pool);
+void readPoolFromFile(std::map<std::string,int> * pool);
+
+#endif
