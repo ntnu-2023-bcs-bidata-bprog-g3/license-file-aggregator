9d10953ce5787a945ec824eac67153349c024314
Kristian Svanholm <KristianRorenSvanholm@gmail.com>
2023-02-07 10:19:10 +0100

    Add error handling to current APIs with helper functions

--- src/controller/Controller.hpp
+++ src/controller/Controller.hpp
@@ -7,6 +7,8 @@
 #include "oatpp/core/macro/codegen.hpp"
 #include "oatpp/core/macro/component.hpp"
 
+#include <boost/algorithm/string.hpp>
+
 #include <iostream>
 #include <string>
 #include <map>
@@ -35,11 +37,14 @@ public:
   
 	ENDPOINT("POST", "/AddLicense", AddLicense, BODY_DTO(Object<License>, license)){
 		// Check if required fields are present.
-		if(!license->name || !license->time){
-			auto response = HelloWorld::createShared();
-			response->message = "Invalid request body.";
-			return createDtoResponse(Status::CODE_400, response);
-		} 
+		if(!license->name || !license->time)
+			return err(Status::CODE_400, "Invalid request body");
+
+		license->name = removeWhitespace(license->name);
+
+		// Check if name is filled and if license is in pool
+		if(license->name == "" || license->time == 0)
+			return err(Status::CODE_400, "Name and time requires data.");
 
 		pool[license->name] += license->time;
 
@@ -50,11 +55,21 @@ public:
 
 	ENDPOINT("DELETE", "/ConsumeLicense", ConsumeLicense, BODY_DTO(Object<License>, license)){
 		// Check if requires fields are present.
-		if(!license->name || !license->time){
-			auto response = HelloWorld::createShared();
-			response->message = "Invalid request body.";
-			return createDtoResponse(Status::CODE_400, response);
-		} 
+		if(!license->name || !license->time)
+			return err(Status::CODE_400, "Invalid request body");
+
+		license->name = removeWhitespace(license->name);
+
+		// Check if name is filled and if license is in pool
+		if(pool.find(license->name) == pool.end())
+			return err(Status::CODE_400, "License not found!");
+
+		// Check if license has enough time left to subtract amount.
+		if(pool[license->name] - license->time < 0){
+			auto msg = "License with name \'" + license->name + "\' does not have enough time left to consume ["
+			+std::to_string(license->time)+"] seconds. (["+std::to_string(pool[license->name])+"] seconds left.)";
+			return err(Status::CODE_400, msg);
+		}
 
 		pool[license->name] -= license->time;
 
@@ -63,6 +78,7 @@ public:
 		return createDtoResponse(Status::CODE_200, licenseList);
 	}
 
+	// Convert entire pool to DTOs
 	LicenseList::Wrapper getPool(){
 		auto licenseList = LicenseList::createShared();
 		oatpp::List<oatpp::Object<License>> list({});
@@ -74,6 +90,20 @@ public:
 		}
 		licenseList->licenses = list;
 		return licenseList;
+	}
+
+	// Create new error response
+	std::shared_ptr<oatpp::web::server::api::ApiController::OutgoingResponse> 
+	err(oatpp::web::protocol::http::Status status, std::string msg){
+		auto error = Error::createShared();
+		error->errormsg = msg;
+		return createDtoResponse(Status::CODE_400, error);
+	}
+
+	std::string removeWhitespace(std::string str){
+		str = boost::trim_left_copy(str);  // Remove prefix spaces
+    	str = boost::trim_right_copy(str); // Remove suffix spaces
+		return str;
 	}
 
 };
