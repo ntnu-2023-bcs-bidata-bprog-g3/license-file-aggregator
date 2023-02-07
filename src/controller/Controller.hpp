0daf8c2590ca25659a118cb08a49057cab8cb55e
Kristian Svanholm <KristianRorenSvanholm@gmail.com>
2023-02-07 09:27:19 +0100

    Two simple APIS for adding to and consuming licenses

--- src/controller/Controller.hpp
+++ src/controller/Controller.hpp
@@ -11,7 +11,6 @@
 #include <string>
 #include <map>
 std::map<std::string, int> pool;
-
 #include OATPP_CODEGEN_BEGIN(ApiController) //<-- Begin Codegen
 
 /**
@@ -19,37 +18,64 @@ std::map<std::string, int> pool;
  */
 class Controller : public oatpp::web::server::api::ApiController {
 public:
-  /**
-   * Constructor with object mapper.
-   * @param objectMapper - default object mapper used to serialize/deserialize DTOs.
-   */
-  Controller(OATPP_COMPONENT(std::shared_ptr<ObjectMapper>, objectMapper))
-    : oatpp::web::server::api::ApiController(objectMapper)
-  {}
+	/**
+	 * Constructor with object mapper.
+	 * @param objectMapper - default object mapper used to serialize/deserialize DTOs.
+	 */
+	Controller(OATPP_COMPONENT(std::shared_ptr<ObjectMapper>, objectMapper))
+		: oatpp::web::server::api::ApiController(objectMapper)
+	{}
 public:
   
-  ENDPOINT("GET", "/", root) {
-    auto dto = HelloWorld::createShared();
-    dto->message = "Hello World!";
-    return createDtoResponse(Status::CODE_200, dto);
-  }
+	ENDPOINT("GET", "/", root) {
+		auto dto = HelloWorld::createShared();
+		dto->message = "Hello World!";
+		return createDtoResponse(Status::CODE_200, dto);
+	}
   
-  ENDPOINT("POST", "/AddLicense", AddLicense, BODY_DTO(Object<License>, license)){
-    pool[license->name] += license->time;
+	ENDPOINT("POST", "/AddLicense", AddLicense, BODY_DTO(Object<License>, license)){
+		// Check if required fields are present.
+		if(!license->name || !license->time){
+			auto response = HelloWorld::createShared();
+			response->message = "Invalid request body.";
+			return createDtoResponse(Status::CODE_400, response);
+		} 
 
-    //std::cout<< license->name << " " << license->time << std::endl;
-    oatpp::List<oatpp::Object<License>> list({});
-    for(auto p: pool){
-      auto l = License::createShared();
-      l->name = p.first;
-      l->time = p.second;
-      list->emplace(list->end(), l);
-    }
-    auto licenseList = LicenseList::createShared();
-    licenseList->licenses = list;
+		pool[license->name] += license->time;
 
-    return createDtoResponse(Status::CODE_200, licenseList);
-  }
+		auto licenseList = getPool();
+
+		return createDtoResponse(Status::CODE_200, licenseList);
+	}
+
+	ENDPOINT("DELETE", "/ConsumeLicense", ConsumeLicense, BODY_DTO(Object<License>, license)){
+		// Check if requires fields are present.
+		if(!license->name || !license->time){
+			auto response = HelloWorld::createShared();
+			response->message = "Invalid request body.";
+			return createDtoResponse(Status::CODE_400, response);
+		} 
+
+		pool[license->name] -= license->time;
+
+		auto licenseList = getPool();
+
+		return createDtoResponse(Status::CODE_200, licenseList);
+	}
+
+	LicenseList::Wrapper getPool(){
+		auto licenseList = LicenseList::createShared();
+		oatpp::List<oatpp::Object<License>> list({});
+		for(auto p: pool){
+			auto l = License::createShared();
+			l->name = p.first;
+			l->time = p.second;
+			list->emplace(list->end(), l);
+		}
+		licenseList->licenses = list;
+		return licenseList;
+	}
+
 };
 
 #include OATPP_CODEGEN_END(ApiController) //<-- End Codegen
