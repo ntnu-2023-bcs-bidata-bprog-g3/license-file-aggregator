fe92aab6310334100e44975a9298f1aa4ac5d06b
Kristian Svanholm <KristianRorenSvanholm@gmail.com>
2023-03-01 10:10:56 +0100

    Update endpoint into proper format

--- src/client/client.hpp
+++ src/client/client.hpp
@@ -7,7 +7,7 @@ class Client : public oatpp::web::client::ApiClient {
 
   API_CLIENT_INIT(Client)
   // TODO:: Change to correct endpoint. Consult Sander on post body etc.
-  API_CALL("GET", "/addLFA", getResource)
+  API_CALL("POST", "api/v1/lfa/register", getResource, QUERY(String, name))
 
 };
 
